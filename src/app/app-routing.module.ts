4162140f0d3e12df548232864b6fa3acbdec5d8e
antoluduenabereziuk21 <antolube20@gmail.com>
2023-03-12 20:19:39 -0300

    added cartComponent more feautures

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
-  {path:'',component:HomeComponent,pathMatch:'full'}
+  {path:'cart',component:CartComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
 ];
 
 @NgModule({
