739c18590285176ae648ec6db2b4f5d1dc5f3a0c
antoluduenabereziuk21 <antolube20@gmail.com>
2023-03-24 19:40:01 -0300

    added in app.component our cartService for update the headerComponent automatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Cart } from './pages/models/cart.model';
+import { CartService } from './services/cart.service';
 @Component({
   selector: 'app-root',
+  /*  Now what we can do here is we can use this cart and pass that 
+  to our header component <app-header [cart]="cart"></app-header>
+  */
   template: `
-  <app-header></app-header>
+  <app-header [cart]="cart"></app-header>
   <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {
-  title = 'store';
+export class AppComponent implements OnInit {
+  /*first create anew cart property the type Cart and let's add 
+  the default value
+  Now we want to implement the NgOnInit, and we  want include our 
+  cart service in the constructor*/
+  cart : Cart ={ items :[]};
+
+  constructor(private cartService: CartService){}
+  /*Now when the component is initializing we want to call cart service
+    and we want to subscribe to our cart Service and it will receive back
+    a card once it gets updated*/
+  ngOnInit(){
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+    });
+  }
 }
